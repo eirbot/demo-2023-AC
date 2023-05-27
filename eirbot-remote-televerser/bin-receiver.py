18294dcf4ec65e84151076fb24be1f969edbaea6
arc4t <arthur.hauswald@gmail.com>
2023-05-27 16:48:49 +0200

    working receiver-sender

--- eirbot-remote-televerser/bin-receiver.py
+++ eirbot-remote-televerser/bin-receiver.py
@@ -1,29 +1,27 @@
 import socket
 
-IP = socket.gethostbyname(socket.gethostname())
-PORT = 4456
-ADDR = ('192.168.43.200', PORT)
+IP = '192.168.43.200'
+PORT = 4455
+ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 
+#receive the file
 def main():
     print("[STARTING] Server is starting...")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server = server.sockopt(socket.SOL_SOCKET, 25, 'wlan0')
     server.bind(ADDR)
     server.listen()
-    print(f"[LISTENING] Server is listening on 192.168.43.200")
-    
-    conn, addr = server.accept()
+    print(f"[LISTENING] Server is listening on {IP}")
+    client, addr = server.accept()
     print(f"[NEW CONNECTION] {addr} connected.")
-    filename = conn.recv(SIZE).decode(FORMAT)
-    file = open(filename, "wb")
-    conn.send("Filename received".encode(FORMAT))
-    data = conn.recv(SIZE).decode(FORMAT)
+    file = open("test.txt", "wb")
+    data = client.recv(SIZE)
     file.write(data)
-    file.close()
     print(f"[{addr}] {data}")
-    conn.close()
+    file.close()
+    client.close()
     
 if __name__ == "__main__":
     main()
+    
--- eirbot-remote-televerser/bin-sender.py
+++ eirbot-remote-televerser/bin-sender.py
@@ -1,25 +1,19 @@
 import socket
 
-IP = socket.gethostbyname(socket.gethostname())
-PORT = 4456
+IP = '192.168.43.200'
+PORT = 4455
 ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 def main():
-    print("[STARTING] Server is starting...")
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
-    server.listen()
-    print(f"[LISTENING] Server is listening on {IP}")
-    while True:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(ADDR)
-        print(f"[NEW CONNECTION] {ADDR} connected.")
-        file = open("test.txt", "rb")
-        data = file.read()
-        client.send(data)
-        print(f"[{ADDR}] {data}")
-        client.close()
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    print(f"[NEW CONNECTION] {ADDR} connected.")
+    file = open("test.txt", "rb")
+    data = file.read()
+    client.send(data)
+    print(f"[{ADDR}] {data}")
+    client.close()
         
 if __name__ == "__main__":
     main()
